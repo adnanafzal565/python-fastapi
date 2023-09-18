63cee9e1c01501a954d7d04e8da117c1a72b3eba
Adnan Afzal <adnanafzal565@gmail.com>
2023-09-18 11:47:02 +0500

    Initial commit

--- api.py
+++ api.py
@@ -6,4 +6,4 @@ def hello_world():
 	return "Hello world"
 
 if __name__ == "__main__":
-	uvicorn.run("main:app", port=8000, host="127.0.0.1", reload=True)
+	uvicorn.run("api:app", port=8000, host="127.0.0.1", reload=True)
